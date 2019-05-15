97f0aac5122ff45596f4a911f6e358383229a243
santiagodelgado1502 <50088704+santiagodelgado1502@users.noreply.github.com>
2019-05-15 08:44:24 -0500

    Add files via upload

---
+++ animalcs.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo3A.clases
+{
+    public abstract class animalcs
+    {
+        public abstract String hacerruido();
+        public virtual void comer()
+        {
+            
+        }
+
+        private String nombre;
+
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        private int peso;
+
+        public int Peso
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+
+        private int edad;
+
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+        public animalcs(String nombre, int peso, int edad)
+        {
+            Nombre = nombre;
+            Peso = peso;
+            Edad = edad;
+        }
+    }
+}
